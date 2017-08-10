ddc9fabde8c603205f410795f7f2ec3384d099a9
Varabe <zertarx@gmail.com>
2017-08-10 17:51:16 +0200

    [add] url for getting card images

--- maker/urls.py
+++ maker/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from maker.views import index
+from maker.views import index, cards
 
 
 urlpatterns = [
 	url(r'^$', index, name="index"),
+	url(r'^cards/(?P<card_id>\d{1,3})$', cards),
 ]
--- maker/views.py
+++ maker/views.py
@@ -1,8 +1,21 @@
+from scripts.lib.database import Database
 from django.http import HttpResponse
 from django.template import loader
+import scripts.lib.cards as cardlib
 
 
 def index(request):
+	database = Database("data/cards/merlin_shortened.csv")
+	all_cards = database.getAll()
+	context = {"all_cards":all_cards}
 	template = loader.get_template("maker/index.html")
-	render = template.render()
+	render = template.render(context, request)
 	return HttpResponse(render)
+
+def cards(request, card_id):
+	card_id = cardlib.formatId(card_id)
+	#TODO: checkpath (not possible through os, probably will have to do through templates)
+	path = f"/static/cards/English/{card_id}.png"
+	response = f"<img src={path}>"
+	return HttpResponse(response)
+		
