20aee67ad0b5563d6f4d6610bfd1b4566819e868
Varabe <zertarx@gmail.com>
2017-08-12 02:54:42 +0300

    [add] card info (+ cost) yielding

--- maker/views.py
+++ maker/views.py
@@ -8,10 +8,29 @@ import os
 def index(request):
 	database = Database("cardbase.csv")
 	all_cards = database.getAll()
-	context = {"all_cards":all_cards}
+	info = yieldCardsInfo(all_cards)
+	context = {"cards":info}
 	template = loader.get_template("maker/index.html")
 	render = template.render(context, request)
 	return HttpResponse(render)
+
+
+def yieldCardsInfo(cards):
+	# Пока что функция уродлива, потом рефакторну
+	land_names = ["L", "M", "F", "D"]
+	for card in cards:
+		land_values = card.lake, card.mountain, card.forest, card.desert
+		lands = tuple(zip(land_names, land_values))
+		lands = [(n, v) for n, v in lands if v is not '']
+		cost = "lands:"
+		for name, value in lands:
+			cost += " %s%s" % (name, value)
+		if cost == "lands:":
+			cost = card.faeria
+		else:
+			cost = "%s, %s" % (card.faeria, cost)
+		yield card.id, card.name, cost 
+
 
 def cards(request, card_id):
 	image = "%s.png" % cardlib.formatId(card_id)
