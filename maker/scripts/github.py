0292eacb284d1cd68b4cf1e84ae6ebe4fc411b94
Varabe <zertarx@gmail.com>
2017-08-15 20:14:36 +0300

    [add] python2 compatibility for github.py and update.py

--- maker/scripts/github.py
+++ maker/scripts/github.py
@@ -1,13 +1,14 @@
-from os import makedirs
 import requests
+import errno
 import json
+import os
 
 api_url = "https://api.github.com"
 
 
 def downloadFolder(owner, repo, folder_path, folder_name, path="."):
 	path = "{}/{}".format(path, folder_name)
-	makedirs(path, exist_ok=True)
+	_makedirs(path)
 	folder = getRepoFolder(owner, repo, folder_path, folder_name)
 	files = _yieldFromFolder(folder)
 	_downloadFiles(files, path)
@@ -26,6 +27,17 @@ def getRepoFolder(owner, repo, folder_path, folder_name):
 	return text
 
 
+def _makedirs(path):
+	""" os.mkdirs(path, exist_ok=True) for python 2 """
+	try:
+		os.makedirs(path)
+	except OSError as exc:  # Python >2.5
+		if exc.errno == errno.EEXIST and os.path.isdir(path):
+			pass
+		else:
+			raise
+
+
 def _yieldFromFolder(folder):
 	for file in folder:
 		yield file['name'], file['download_url']
@@ -36,3 +48,4 @@ def _downloadFiles(files, path):
 		file_path = "{}/{}".format(path, name)
 		downloadFile(download_url, file_path)
 		print(name + " downloaded")
+
