c69f686891dc02a8cb6b1162c2a164e2aa471891
Varabe <zertarx@gmail.com>
2017-08-15 18:04:09 +0300

    Abstract card logic from cardbase

--- scripts/lib/cardbase.py
+++ scripts/lib/cardbase.py
@@ -1,15 +1,7 @@
 from csv import reader as csv_reader
-from collections import namedtuple
-import os
 
 from scripts.lib.errors import CardbaseError
-
-
-USELESS_CARD_FIELDS = 15, 14, 13, 12, 4 
-IMAGE_TEMPLATE = "/static/cards/English/%s.png"
-IMAGE_NOT_FOUND = "/static/cards/notfound.png"
-Card = namedtuple("Card", field_names="id color name type faeria lake mountain forest desert power life rarity image")
-
+from scripts.lib.cards import Card
 
 class Cardbase:
 	def __init__(self, path):
@@ -42,32 +34,4 @@ class Cardbase:
 	def _getContents(self):
 		with open(self.path) as csvfile:
 			reader = csv_reader(csvfile, delimiter=';')
-			return tuple(extractCard(row) for row in reader)
-
-
-""" all_card_fields = "id color name type _ faeria lake mountain forest desert power life effects _ count_in_codex codex_id rarity" """
-def extractCard(fields):
-	for field in USELESS_CARD_FIELDS:
-		fields.pop(field)
-	card_id = formatId(fields[0])
-	fields.append(getImage(card_id))
-	return Card(*fields)
-
-
-def formatId(card_id):
-	card_id = str(card_id)
-	size = len(card_id)
-	if size == 1:
-		return "00%s" % card_id
-	elif size == 2:
-		return "0%s" % card_id
-	else:
-		return card_id
-
-
-def getImage(card_id):
-	django_path = IMAGE_TEMPLATE % card_id
-	real_path = "frontend" + django_path
-	if not os.path.exists(real_path):
-		django_path = IMAGE_NOT_FOUND
-	return django_path
+			return tuple(Card(row) for row in reader)
--- scripts/lib/cards.py
+++ scripts/lib/cards.py
@@ -1,54 +1,36 @@
-# from PIL import Image, ImageFont, ImageDraw
-# from lib.properties import ImageProperty
+from collections import namedtuple
+import os
 
 
-# card_font = ImageFont.truetype("/home/varabe/.fonts/truetype/Baskerville/LibreBaskerville-Bold.otf", 75)
+USELESS_CARD_FIELDS = 15, 14, 13, 12, 4 
+IMAGE_TEMPLATE = "/static/cards/English/%s.png"
+IMAGE_NOT_FOUND = "/static/cards/notfound.png"
+Card_metaclass = namedtuple("Card", field_names="id color name type faeria lake mountain forest desert power life rarity image")
+""" all_card_fields = "id color name type _ faeria lake mountain forest desert power life effects _ count_in_codex codex_id rarity" """
 
 
-# class CardProperty(ImageProperty):
-# 	font = card_font
-# 	color = (255, 255, 255)
-# 	def getXOffset(self):
-# 		value = self.value
-# 		if 1 < value < 10:
-# 			return -6
-# 		elif value == 0:
-# 			return -12
-# 		elif value >= 10:
-# 			return -26
+def Card(card_fields):
+	for field in USELESS_CARD_FIELDS:
+		card_fields.pop(field)
+	card_id = formatId(card_fields[0])
+	card_fields.append(getImage(card_id))
+	return Card_metaclass(*card_fields)
 
 
-# class Life(CardProperty):
-# 	box = (655, 877)
-# 	x, y = 38, -3
-# 	image = Image.open("data/heart.png")
+def formatId(raw_card_id):
+	card_id = str(raw_card_id)
+	size = len(card_id)
+	if size == 1:
+		return "00%s" % card_id
+	elif size == 2:
+		return "0%s" % card_id
+	else:
+		return card_id
 
 
-# class Power(CardProperty):
-# 	box = (258, 834)
-# 	x, y = 34, 43
-# 	image = Image.open("data/power.png")
-
-
-# class Faeria(CardProperty):
-# 	box = (246, 181)
-# 	x, y = 30, -1
-# 	color = (0, 69, 96)
-# 	image = Image.open("data/faeria.png")
-
-
-# class Card(CardInfo):
-# 	def __init__(self, *args):
-# 		self.life = Life(self.std_life, parent=self)
-# 		self.power = Power(self.std_power, parent=self)
-# 		self.faeria = Faeria(self.std_faeria, parent=self)
-
-# 	def openImage(self):
-# 		id_ = formatId(self.id)
-# 		try:
-# 			self.image = Image.open(f"data/cards/English/{id_}.png")
-# 		except FileNotFoundError as e:
-# 			raise CardError(f"Card ID:{id_} image not found.") from e
-
-
-
+def getImage(card_id):
+	django_path = IMAGE_TEMPLATE % card_id
+	real_path = "frontend" + django_path
+	if not os.path.exists(real_path):
+		django_path = IMAGE_NOT_FOUND
+	return django_path
