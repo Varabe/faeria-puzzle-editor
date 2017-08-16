2f550b45bd26f6b41140287121e29f958f2ca1a5
Varabe <zertarx@gmail.com>
2017-08-17 01:12:43 +0300

    [add] ignoring files of proper format in cardimages.resize

--- maker/scripts/cardimages.py
+++ maker/scripts/cardimages.py
@@ -2,15 +2,16 @@ from PIL import Image
 
 
 STD_CROP_DIMENSIONS = 215, 105, 800, 1005
-STD_THUMBNAIL_SIZE = 300, 462
+STD_THUMBNAIL_SIZE = 300, 461 # Not safe to change numbers, they depend on each other
 
 
 def resize(path, new_path=None, do_thumbnail=False):
 	new_path = new_path or path
 	image = Image.open(path)
-	image = _crop(image)
-	if do_thumbnail: _thumbnail(image)
-	image.save(new_path)
+	if (image.width, image.height) != STD_THUMBNAIL_SIZE:
+		image = _crop(image)
+		if do_thumbnail: _thumbnail(image)
+		image.save(new_path)
 	image.close()
 
 
