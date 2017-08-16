af6d9c8bcc3cdbda99e0f56e1aeb1d06de0129dc
Varabe <zertarx@gmail.com>
2017-08-16 16:42:23 +0300

    Replace Card.gold with Card.wild Card class now accepts all card_fields

--- maker/scripts/cards.py
+++ maker/scripts/cards.py
@@ -6,14 +6,13 @@ LAND_FIELDS = 5, 6, 7, 8
 USELESS_CARD_FIELDS = 15, 14, 13, 12, 4
 IMAGE_TEMPLATE = "/static/cards/English/%s.png"
 IMAGE_NOT_FOUND = "/static/cards/notfound.png"
-Card_metaclass = namedtuple("Card", field_names="id color name type faeria lake forest mountain desert power life rarity image")
-""" all_card_fields = "id color name type gold faeria lake forest mountain desert power life text codex count_in_codex codex_id rarity" """
+CARD_FIELDS = "id color name type wild faeria lake forest mountain desert power life text codex count_in_codex codex_id rarity image"
+Card_metaclass = namedtuple("Card", field_names=CARD_FIELDS)
+
 
 def Card(card_fields):
 	for field_id in LAND_FIELDS:
 		card_fields[field_id] = card_fields[field_id] or "0"
-	for field_id in USELESS_CARD_FIELDS:
-		card_fields.pop(field_id)
 	card_id = formatId(card_fields[0])
 	card_fields.append(getImage(card_id))
 	return Card_metaclass(*card_fields)
