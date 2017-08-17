5e497507b40a8104f2d9bb98cf0b65f15a6b9158
Bobrokrot <bobrokrot@gmail.com>
2017-08-17 19:38:59 +0300

    [add] remove button on image hover

--- frontend/static/maker/js/hand.js
+++ frontend/static/maker/js/hand.js
@@ -9,12 +9,20 @@ function addToHand(card) {
 		icon = $('<i class="glyphicon glyphicon-remove"></i>'),
 		button = $('<button onclick="removeFromHand(\'' + card.name + '\')" type="button" class="btn pull-right mywell"></button>'),
 		elem2 = $('<li class="list-inline-item"></li>'),
-		img = $('<img class="card_in_hand" name="' + card.name +'">');
+		div = $('<div class="card_in_hand container"></div>'),
+		img = $('<img name="' + card.name +'">'),
+		overlay = $('<div class="overlay"></div>');
+	
 	button.append(icon);
 	elem.append(button);
 	button.before(text);
+	
 	img.attr('src', card.image);
-	elem2.append(img);
+	elem2.append(div);
+	div.append(img);
+	overlay.append(button.clone().removeClass("pull-right"));
+	div.append(overlay);
+	
 	$('#add_button').before(elem);
 	$('#img_list').append(elem2);
 	$('.modal-footer button').click();
@@ -22,5 +30,5 @@ function addToHand(card) {
 
 function removeFromHand(name) {
 	$('#cards_in_hand .card_list_item:contains('+name+')').remove();
-	$('.card_in_hand[name="'+ name +'"]').parent().remove();
+	$('#img_list .card_in_hand img[name="'+ name +'"]').parent().remove();
 }
