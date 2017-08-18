20226c5ecfde2f99152d80f1eb5c3ab96e0a0515
Varabe <zertarx@gmail.com>
2017-08-18 03:59:11 +0300

    Remove '_' prefix from methods for it's unneeded

--- maker/scripts/cardimages.py
+++ maker/scripts/cardimages.py
@@ -3,24 +3,25 @@ from PIL import Image
 """ Not safe to change numbers, they depend on each other """
 STD_CROP_DIMENSIONS = 205, 0, 810, 1024
 STD_THUMBNAIL_SIZE = 300, 507
+350, 295, 330, 330
 
 
 def resize(path, new_path=None, do_thumbnail=False):
 	new_path = new_path or path
 	image = Image.open(path)
 	if (image.width, image.height) != STD_THUMBNAIL_SIZE:
-		image = _crop(image)
-		if do_thumbnail: _thumbnail(image)
+		image = crop(image)
+		if do_thumbnail: thumbnail(image)
 		image.save(new_path)
 	image.close()
 
 
-def _crop(image, dims=STD_CROP_DIMENSIONS):
+def crop(image, dims=STD_CROP_DIMENSIONS):
 	try:
 		return image.crop(dims)
 	except OSError:
 		raise OSError(image.filename + " is damaged")
 
 
-def _thumbnail(image, size=STD_THUMBNAIL_SIZE):
+def thumbnail(image, size=STD_THUMBNAIL_SIZE):
 	image.thumbnail(size, Image.ANTIALIAS)
