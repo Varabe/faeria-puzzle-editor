998d3e971820fb24d149135557ecca47499b5b2c
Bobrokrot <bobrokrot@gmail.com>
2017-08-20 18:40:48 +0300

    [add] some foundation for board developement

--- frontend/static/maker/js/lands.js
+++ frontend/static/maker/js/lands.js
@@ -1,5 +1,32 @@
 
-var side = 1;
+var landCoord = "D6";
+
+function Land() {
+	this.type = 'ocean';
+	this.side = 1;
+	this.setType = function (type) {
+		this.type = type;
+	};
+	this.setSide = function (side) {
+		this.side = side;
+	};
+}
+
+var LandArray = {
+	A2: new Land(), A3: new Land(),
+	B1: new Land(), B2: new Land(), B3: new Land(), B4: new Land(), B5: new Land(),
+	C1: new Land(), C2: new Land(), C3: new Land(), C4: new Land(), C5: new Land(), C6: new Land(),
+	D2: new Land(), D3: new Land(), D4: new Land(), D5: new Land(), D6: new Land(),
+	E1: new Land(), E2: new Land(), E3: new Land(), E4: new Land(), E5: new Land(), E6: new Land(),
+	F1: new Land(), F2: new Land(), F3: new Land(), F4: new Land(), F5: new Land(),
+	G2: new Land(), G3: new Land()
+};
+
+LandConst = {
+	x0: 546, y0: 272,
+	dx1: 81, dx2: 39, dx: 81 + 39,
+	dy1: 49, dy: 98
+};
 
 $('html').keydown(function (event) {
 	switch(event.which){
@@ -20,23 +47,54 @@ $('html').keydown(function (event) {
 	}
 });
 
-function createLand(landtype) {
+function setLandCoord( lc ) {
+	landCoord = lc;
+	var xy = getLandXY(lc);
+	$('.land_coordinates span').text(landCoord + " x: " + xy.x + " y: " + xy.y);
+	setLandPills(LandArray[lc].type);
+	setSwitch(LandArray[lc].side);
+}
+
+function getLandXY( lc ) {
+	var d, n, x, y, letter = lc[0], number = lc[1];
+	switch(letter) {
+		case "A": n = 0; d = 0; break;
+		case "B": n = 1; d = 1; break;
+		case "C": n = 2; d = 2; break;
+		case "D": n = 3; d = 3; break;
+		case "E": n = 4; d = 2; break;
+		case "F": n = 5; d = 1; break;
+		case "G": n = 6; d = 0; break;
+		default: break;
+	}
+	x = LandConst.x0 + n*LandConst.dx;
+	y = LandConst.y0 + (number - 1)*LandConst.dy - d*LandConst.dy1;
+	return {x: x, y: y};
+}
+
+function setLandPills(landtype) {
 	$('#lands .mypills li[class=active]').removeClass('active');
 	$('#lands .mypills a[type=' + landtype + '').parent().addClass('active');
 }
 
-function switchSide() {
+function setSwitch(sd) {
 	var img = $('#lands .mypills a[type=switch] img'),
-		parent = img.parent(),
-		src = img.prop('src'),
-		len = src.length;
-	if (src[len - 5] == '1') { //ends with switch1.png
-		img.remove();
-		parent.append($('#switch2').clone());
-		side = 2;
+		parent = img.parent();
+	img.remove();
+	parent.append($('#switch' + sd).clone());
+	
+}
+function createLand(landtype) {
+	setLandPills(landtype);
+	LandArray[landCoord].setType(landtype);
+}
+
+function switchSide() {
+	if (LandArray[landCoord].side == 1) {
+		LandArray[landCoord].setSide(2);
+		setSwitch(2);
 	} else {
-		img.remove();
-		parent.append($('#switch1').clone());
-		side = 1;
+		LandArray[landCoord].setSide(1);
+		setSwitch(1);
 	}
 }
