d00899f25dd3cf752c571b76141ae946da17f53b
Varabe <zertarx@gmail.com>
2017-08-23 20:31:49 -0400

    [ref] spaces -> tabs

--- faeria_puzzle/urls.py
+++ faeria_puzzle/urls.py
@@ -1,17 +1,17 @@
 """faeria_puzzle URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+	https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+	1. Add an import:  from my_app import views
+	2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+	1. Add an import:  from other_app.views import Home
+	2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+	1. Import the include() function: from django.conf.urls import url, include
+	2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
 from django.contrib import admin
@@ -21,6 +21,6 @@ from faeria_puzzle.views import index
 
 urlpatterns = [
 	url(r'^$', index, name="index"),
-    url(r'^maker/', include('maker.urls')),
-    url(r'^admin/', admin.site.urls),
+	url(r'^maker/', include('maker.urls')),
+	url(r'^admin/', admin.site.urls),
 ]
