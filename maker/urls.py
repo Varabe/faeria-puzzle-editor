39e1835f714aa4a4df3506d131cc01bae7256340
Varabe <zertarx@gmail.com>
2017-08-27 01:16:27 -0400

    [add] maker/card url to get card information

--- maker/urls.py
+++ maker/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from maker.views import index
+from maker.views import index, get_card
 
 
 urlpatterns = [
 	url(r'^$', index, name="index"),
+	url(r'^card/(?P<card_id>\d{1,3})/$', get_card, name="card"),
 ]
--- maker/views.py
+++ maker/views.py
@@ -1,5 +1,6 @@
-from maker.scripts.cards.cardbase import Cardbase
+from maker.scripts.cards.cardbase import Cardbase, CardbaseError
 from django.shortcuts import render
+from django.http import JsonResponse
 
 cardbase = Cardbase("cardbase.csv")
 
@@ -7,3 +8,11 @@ cardbase = Cardbase("cardbase.csv")
 def index(request):
 	cards = cardbase.get_all()
 	return render(request, "maker/index.html", context={"cards":cards})
+
+
+def get_card(request, card_id):
+	try:
+		card = cardbase.get_by_id(card_id)
+	except CardbaseError:
+		return JsonResponse({"error":"Card not found"}, status=404)
+	return JsonResponse(card)
